be5fbbaeefe6ab5ce6591f285bad668d4e890cf7
Saqib Mushtaq <i.saqibmushtaq@gmail.com>
2025-09-28 17:09:51 +0500

    added page response

---
+++ school-api/src/main/java/com/saqib/school/common/dto/PageResponse.java
@@ -0,0 +1,54 @@
+package com.saqib.school.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+  private List<T> content;
+  private int page;
+  private int size;
+  private long totalElements;
+  private int totalPages;
+  private boolean first;
+  private boolean last;
+  private boolean hasNext;
+  private boolean hasPrevious;
+
+
+  public static <T> PageResponse<T> from(Page<T> page) {
+    return PageResponse.<T>builder()
+      .content(page.getContent())
+      .page(page.getNumber())
+      .size(page.getSize())
+      .first(page.isFirst())
+      .last(page.isLast())
+      .hasNext(page.hasNext())
+      .hasPrevious(page.hasPrevious())
+      .totalPages(page.getTotalPages())
+      .totalElements(page.getTotalElements())
+      .build();
+  }
+
+  public static <T, D> PageResponse<D> from(Page<T> page, List<D> content) {
+    return PageResponse.<D>builder()
+      .content(content)
+      .page(page.getNumber())
+      .size(page.getSize())
+      .first(page.isFirst())
+      .last(page.isLast())
+      .hasNext(page.hasNext())
+      .hasPrevious(page.hasPrevious())
+      .totalPages(page.getTotalPages())
+      .totalElements(page.getTotalElements())
+      .build();
+  }
+}
